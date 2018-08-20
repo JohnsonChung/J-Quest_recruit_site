09365a18dde9aca7553b23fd7b34119390fa8896
JohnsonChung <johnson780510@gmail.com>
2018-08-20 20:49:27 +0800

    update doc with 8/11 update sheet

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["bower_components/webcomponentsjs/custom-elements-es5-adapter.js","a5043c1d0dd16d84558ee6cc2276212e"],["bower_components/webcomponentsjs/gulpfile.js","0366da1f0f7858c9af2daa3ef7d950ea"],["bower_components/webcomponentsjs/webcomponents-hi-ce.js","e0655b55c34560830d28eda5ec51726c"],["bower_components/webcomponentsjs/webcomponents-hi-sd-ce.js","ffe437e8a8797cef96b23ced1591b7eb"],["bower_components/webcomponentsjs/webcomponents-hi.js","b88ce284fda849097ab4997d97bfc8b6"],["bower_components/webcomponentsjs/webcomponents-lite.js","91af6c96521441dd2349d3a0fa83923c"],["bower_components/webcomponentsjs/webcomponents-loader.js","f13bbbbf647b7922575a7894367ddaaf"],["bower_components/webcomponentsjs/webcomponents-sd-ce.js","2b3ce5380128f1e0e3ad8a130d68cf2a"],["index.html","77334dbc6bb69ae710231509ff59f767"],["manifest.json","39a400389f05600d3ec8c419680017c6"],["src/lazy-resources.html","397c8e2ca9425202a48b50311981940b"],["src/my-app.html","ef0c3170c524612a30e146265f870b18"],["src/my-graduate-blog.html","85da3057ad926a825ffa48e5da13a4a1"],["src/my-graduate-career.html","600be9f2511ec6e9ceb43a708f9b8ed9"],["src/my-graduate-details.html","aaf147e13fc59db6b14740cc563d1928"],["src/my-graduate-interview.html","f554af82e5e6125462e18ca3b2d36deb"],["src/my-graduate-process.html","e63b689ba597f8f6bff329f0505b1f9a"],["src/my-graduate.html","611ee2d9d952d77dcde7aee0e8fc9984"],["src/my-midterm.html","6bd00af907c55a7e88b00032013c7f0f"],["src/my-nav-data.html","16d70e200d55e05ab953165dc544ee46"],["src/my-parttime-dangerous.html","226f81e7c1c307d2ec071203a4195ebd"],["src/my-parttime-merit.html","f1d3612a833e16200c9940bbe837fef2"],["src/my-parttime-quizanswer.html","a802a992927b01b914a4640c756b2194"],["src/my-parttime-recruitform.html","74d63c14dab49706672e686e1446dd11"],["src/my-parttime-stores.html","d5b8f4b0c3aae1c2576d6bb2a9be69eb"],["src/my-parttime-voice.html","1b81e26cbc8aa3679a737f12881756eb"],["src/my-parttime-works.html","36d91a6f99615757f0b0d14ad5443675"],["src/my-parttime.html","c2cd59e35189e0310bf854dc239b2332"],["src/my-top.html","e801c17d44fc31b231eb7f62dc0e08ab"],["src/my-view3.html","1db06518f8ac8cc14f47b19070814ddb"],["src/my-view404.html","bf96b84a62256a31009759b0644549c7"]];
+var precacheConfig = [["bower_components/webcomponentsjs/custom-elements-es5-adapter.js","a5043c1d0dd16d84558ee6cc2276212e"],["bower_components/webcomponentsjs/gulpfile.js","0366da1f0f7858c9af2daa3ef7d950ea"],["bower_components/webcomponentsjs/webcomponents-hi-ce.js","e0655b55c34560830d28eda5ec51726c"],["bower_components/webcomponentsjs/webcomponents-hi-sd-ce.js","ffe437e8a8797cef96b23ced1591b7eb"],["bower_components/webcomponentsjs/webcomponents-hi.js","b88ce284fda849097ab4997d97bfc8b6"],["bower_components/webcomponentsjs/webcomponents-lite.js","91af6c96521441dd2349d3a0fa83923c"],["bower_components/webcomponentsjs/webcomponents-loader.js","f13bbbbf647b7922575a7894367ddaaf"],["bower_components/webcomponentsjs/webcomponents-sd-ce.js","2b3ce5380128f1e0e3ad8a130d68cf2a"],["index.html","87d9b831c19acce85db56f42e8593498"],["manifest.json","39a400389f05600d3ec8c419680017c6"],["src/lazy-resources.html","c59d35570e128a3aeeee41dcac1130f7"],["src/my-app.html","7496519ac0e9810b81fb84e45bf82854"],["src/my-comeback.html","71e8239de20e763fc1fee57e672c920d"],["src/my-graduate-blog.html","6f8b511816b1d0c33891c0aae355a95e"],["src/my-graduate-career.html","c5c333b6ef07a6fe0e10ace27b5dfc2a"],["src/my-graduate-details.html","f59e7a17c5c963c93128ecd8da531c55"],["src/my-graduate-interview.html","b875c9afaab6ed3290dcd83185a670ba"],["src/my-graduate-process.html","a18eb8968c28810f13f21474233573f9"],["src/my-graduate.html","59682efebf6742991b57cd9c3d602769"],["src/my-midterm-interview.html","5210737dfdd60a1bd322bfcf0b169e19"],["src/my-midterm-process.html","d43f9a860e0f2568f25eed5c15d3b3ab"],["src/my-midterm-works.html","36942b84211ac6484faa7a202e78ae15"],["src/my-midterm.html","8c45e36c1dc864d3e084591f60de0b66"],["src/my-nav-data.html","d89201edfb098318802df5d053e1a792"],["src/my-parttime-dangerous.html","b637319eb542cb66d65daea358d47352"],["src/my-parttime-merit.html","5425a4db05031c2bb3bb05711dc86c61"],["src/my-parttime-quizanswer.html","ecdfc847fd58fe92d8c5d7140e98894d"],["src/my-parttime-recruitform.html","2c15b4d99594dd0ac85101c193786ea8"],["src/my-parttime-stores.html","c842aab7ef4f1160f46f71c400ee72ee"],["src/my-parttime-voice.html","348f1a2c595bba99855cca06a2047631"],["src/my-parttime-works.html","a652a4d5bd4be040032a05302cb128f1"],["src/my-parttime.html","438790388bad131039ef93d7084f9b3e"],["src/my-top.html","0c5744eb684422eea174185339fd9547"],["src/my-view3.html","2600c15c3ccf6aad0c64ac6e72b868fa"],["src/my-view404.html","375707f4af3f006d2a9adc97f137ae76"],["src/table-comeback.html","bd5746acb3e137bf56d378148c164cbf"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
@@ -65,7 +65,7 @@ var cleanResponse = function (originalResponse) {
       Promise.resolve(originalResponse.body) :
       originalResponse.blob();
 
-    return bodyPromise.then(function(body) {
+    return bodyPromise.then(function (body) {
       // new Response() is happy when passed either a stream or a Blob.
       return new Response(body, {
         headers: originalResponse.headers,
@@ -99,7 +99,7 @@ var isPathWhitelisted = function (whitelist, absoluteUrlString) {
 
     // Otherwise compare each path regex to the path of the URL passed in.
     var path = (new URL(absoluteUrlString)).pathname;
-    return whitelist.some(function(whitelistedPathRegex) {
+    return whitelist.some(function (whitelistedPathRegex) {
       return path.match(whitelistedPathRegex);
     });
   };
@@ -112,15 +112,15 @@ var stripIgnoredUrlParameters = function (originalUrl,
 
     url.search = url.search.slice(1) // Exclude initial '?'
       .split('&') // Split into an array of 'key=value' strings
-      .map(function(kv) {
+      .map(function (kv) {
         return kv.split('='); // Split each 'key=value' string into a [key, value] array
       })
-      .filter(function(kv) {
-        return ignoreUrlParametersMatching.every(function(ignoredRegex) {
+      .filter(function (kv) {
+        return ignoreUrlParametersMatching.every(function (ignoredRegex) {
           return !ignoredRegex.test(kv[0]); // Return true iff the key doesn't match any of the regexes.
         });
       })
-      .map(function(kv) {
+      .map(function (kv) {
         return kv.join('='); // Join each [key, value] array into a 'key=value' string
       })
       .join('&'); // Join the array of 'key=value' strings into a string with '&' in between each
@@ -131,7 +131,7 @@ var stripIgnoredUrlParameters = function (originalUrl,
 
 var hashParamName = '_sw-precache';
 var urlsToCacheKeys = new Map(
-  precacheConfig.map(function(item) {
+  precacheConfig.map(function (item) {
     var relativeUrl = item[0];
     var hash = item[1];
     var absoluteUrl = new URL(relativeUrl, self.location);
@@ -141,25 +141,25 @@ var urlsToCacheKeys = new Map(
 );
 
 function setOfCachedUrls(cache) {
-  return cache.keys().then(function(requests) {
-    return requests.map(function(request) {
+  return cache.keys().then(function (requests) {
+    return requests.map(function (request) {
       return request.url;
     });
-  }).then(function(urls) {
+  }).then(function (urls) {
     return new Set(urls);
   });
 }
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return setOfCachedUrls(cache).then(function(cachedUrls) {
+    caches.open(cacheName).then(function (cache) {
+      return setOfCachedUrls(cache).then(function (cachedUrls) {
         return Promise.all(
-          Array.from(urlsToCacheKeys.values()).map(function(cacheKey) {
+          Array.from(urlsToCacheKeys.values()).map(function (cacheKey) {
             // If we don't have a key matching url in the cache already, add it.
             if (!cachedUrls.has(cacheKey)) {
               var request = new Request(cacheKey, {credentials: 'same-origin'});
-              return fetch(request).then(function(response) {
+              return fetch(request).then(function (response) {
                 // Bail out of installation unless we get back a 200 OK for
                 // every request.
                 if (!response.ok) {
@@ -167,7 +167,7 @@ self.addEventListener('install', function(event) {
                     'response with status ' + response.status);
                 }
 
-                return cleanResponse(response).then(function(responseToCache) {
+                return cleanResponse(response).then(function (responseToCache) {
                   return cache.put(cacheKey, responseToCache);
                 });
               });
@@ -175,7 +175,7 @@ self.addEventListener('install', function(event) {
           })
         );
       });
-    }).then(function() {
+    }).then(function () {
       
       // Force the SW to transition from installing -> active state
       return self.skipWaiting();
@@ -184,21 +184,21 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   var setOfExpectedUrls = new Set(urlsToCacheKeys.values());
 
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.keys().then(function(existingRequests) {
+    caches.open(cacheName).then(function (cache) {
+      return cache.keys().then(function (existingRequests) {
         return Promise.all(
-          existingRequests.map(function(existingRequest) {
+          existingRequests.map(function (existingRequest) {
             if (!setOfExpectedUrls.has(existingRequest.url)) {
               return cache.delete(existingRequest);
             }
           })
         );
       });
-    }).then(function() {
+    }).then(function () {
       
       return self.clients.claim();
       
@@ -207,7 +207,7 @@ self.addEventListener('activate', function(event) {
 });
 
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   if (event.request.method === 'GET') {
     // Should we call event.respondWith() inside this fetch event handler?
     // This needs to be determined synchronously, which will give other fetch
@@ -242,14 +242,14 @@ self.addEventListener('fetch', function(event) {
     // event.respondWith(), using the appropriate cache key.
     if (shouldRespond) {
       event.respondWith(
-        caches.open(cacheName).then(function(cache) {
-          return cache.match(urlsToCacheKeys.get(url)).then(function(response) {
+        caches.open(cacheName).then(function (cache) {
+          return cache.match(urlsToCacheKeys.get(url)).then(function (response) {
             if (response) {
               return response;
             }
             throw Error('The cached response that was expected is missing.');
           });
-        }).catch(function(e) {
+        }).catch(function (e) {
           // Fall back to just fetch()ing the request if some unexpected error
           // prevented the cached response from being valid.
           console.warn('Couldn\'t serve response for "%s" from cache: %O', event.request.url, e);
